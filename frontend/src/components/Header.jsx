82c1e389b683ee8519b6ad9a4d3a7b50d4df8a88
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-03 00:54:36 +0530

    added product component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">TechieShop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="" />
+            TechieShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import "./assets/styles/index.css";
+// import "./assets/styles/bootstrap.custom.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
