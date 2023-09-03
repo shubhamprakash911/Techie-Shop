e4b36fb575ff098ed9a087c2dd8558ee798fcab9
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-03 17:16:41 +0530

    successfully connected database to our app

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const isConnect = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected: ${isConnect.connection.host}`);
+  } catch (error) {
+    console.log(`Error ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+connectDB(); //connected to database
 
 app.get("/api/products", (req, res) => {
   res.json(products);
@@ -18,5 +21,5 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
