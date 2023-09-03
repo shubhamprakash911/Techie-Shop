27ce2f59720d9c70d06a99e1905b1be230adf861
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-03 13:04:23 +0530

    intilize backned with express nodemon

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import products from "./data/products.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
