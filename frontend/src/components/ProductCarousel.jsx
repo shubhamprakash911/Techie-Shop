67c08694fb11f671da78591b17172376fdec24d0
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-12 13:45:50 +0530

    remove loader from carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -4,29 +4,35 @@ import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
-  const { data: products, error } = useGetTopProductsQuery();
+  const { data: products, error, isLoading } = useGetTopProductsQuery();
 
   return error ? (
     <Message variant="danger">{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause="hover" interval={2000} className="mb-4">
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image
-              src={product.image}
-              alt={product.name}
-              fluid
-              style={{ display: "block", margin: "auto" }}
-            />
-            <Carousel.Caption className="carousel-caption">
-              <h2 className="text-white text-right">
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
+      {isLoading ? null : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        products.map((product) => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image
+                src={product.image}
+                alt={product.name}
+                fluid
+                style={{ display: "block", margin: "auto" }}
+              />
+              <Carousel.Caption className="carousel-caption">
+                <h2 className="text-white text-right">
+                  {product.name} (${product.price})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))
+      )}
     </Carousel>
   );
 };
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -34,7 +34,7 @@ const HomeScreen = () => {
           <Meta />
           <h1>Latest Products</h1>
           <Row>
-            {data.products.map((product) => (
+            {data?.products?.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
