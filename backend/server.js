ae63b1ff0067730dd66a4f7eab97bf6eec470762
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-12 12:27:33 +0530

    join frontend and backend for production

--- backend/server.js
+++ backend/server.js
@@ -29,17 +29,19 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.status(200).json("Welcome to Techishop backend");
-//   });
-// }
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json("Welcome to Techishop backend");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
