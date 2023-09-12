1917917ed77ba3c3b08fa4eaa54f2ce50d9eee89
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-09-12 13:01:53 +0530

    remove loader from carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
